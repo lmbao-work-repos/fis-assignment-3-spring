edbeddf560879d3ff89c678307802f1b952ba72a
Lê Minh Bảo <besilent308@gmail.com>
2025-01-10 23:05:19 +0700

    Initial commit

---
+++ src/main/java/fis/baolm2/assignment/warehouse_management/FisAssignment3SpringApplication.java
@@ -0,0 +1,13 @@
+package fis.baolm2.assignment.warehouse_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisAssignment3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FisAssignment3SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fis/baolm2/assignment/warehouse_management/FisAssignment3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.baolm2.assignment.warehouse_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisAssignment3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
